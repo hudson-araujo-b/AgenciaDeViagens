bb283172c48d3a43c416a294b3ecbd37533feca0
Hudson <hudson.b.araujo@gmail.com>
2026-06-05 13:31:39 -0300

    feat: criando model de usuário

---
+++ AgenciaDeViagens/Models/UsuarioModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgenciaDeViagens.Models
+{
+    public class UsuarioModel
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string nome { get; set; } = string.Empty;
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido.")]
+        public string email { get; set; } = string.Empty;
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        public string senhaHash { get; set; } = string.Empty;
+    }
+}
