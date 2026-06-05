85ed2a7ae66f098253c89f1e307c1b4e918e7a98
Hudson <hudson.b.araujo@gmail.com>
2026-06-05 13:45:05 -0300

    feat: implementando método de criar conta com repositório e interface

---
+++ AgenciaDeViagens/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using AgenciaDeViagens.Models;
+
+namespace AgenciaDeViagens.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        public void criarConta(UsuarioModel usuario);
+    }
+}
---
+++ AgenciaDeViagens/Repository/UsuarioRepositorio.cs
@@ -0,0 +1,35 @@
+﻿using AgenciaDeViagens.Models;
+using AgenciaDeViagens.Interfaces;
+using MySql.Data.MySqlClient;
+
+namespace AgenciaDeViagens.Repository
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly string _connectionString;
+
+        public UsuarioRepositorio(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("Conexao")!;
+        }
+
+        public void criarConta(UsuarioModel usuario)
+        {
+            using (var conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                string senhaHash = BCrypt.Net.BCrypt.HashPassword(usuario.senhaHash);
+
+                var sql = "INSERT INTO usuario (nome,email,senha_hash) VALUES (@nome,@email,@senha)";
+                var cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@nome", usuario.nome);
+                cmd.Parameters.AddWithValue("@email", usuario.email);
+                cmd.Parameters.AddWithValue("@senha", senhaHash);
+                cmd.ExecuteNonQuery();
+
+            }
+
+        }
+    }
+}
